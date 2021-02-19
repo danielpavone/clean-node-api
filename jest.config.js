635f638a5744ea15d6fa77b5673ab1ac46393e03
Daniel Pavone <danielpavone@gmail.com>
2021-02-19 02:07:08 -0300

    chore: add configuration to mongo in memory

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     '!**/protocols/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
